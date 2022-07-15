4e268daf904d773e0df61e0691bd57be1dd1e6d4
Kaung Myat Linn <kmlinn.thaton@gmail.com>
2022-07-15 09:20:51 +0630

    modified AuthWidget

--- lib/utils/strings.dart
+++ lib/utils/strings.dart
@@ -1,3 +1,12 @@
 class StringUtils {
   static const AppName = "shwelar";
+  static const LimitlessCasinoExperience = "A Limitless Casino Experience";
+  static const RightsReserved = "© 2021 Shan234. All rights reserved.";
+
+  static const StartPlaying = "Start Playing";
+  static const AccountLogin = "Account Login";
+  static const UserName = "Username";
+  static const Password = "Password";
+  static const ForgotPassword = "Forgot Password?";
+  static const EnterGame = "Enter Game";
 }
